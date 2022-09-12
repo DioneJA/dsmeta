a93beaf9350d8c2b533937816ed4bff7195dc242
DioneJa <brunoalves2017@gmail.com>
2022-09-12 16:22:18 -0300

    ToastFy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import './index.css';
 import SalesCard from './components/SalesCard';
 import Header from "./components/Header";
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+    <ToastContainer/>
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/Notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -9,7 +10,7 @@ type Props = {
 
 function handleClick(saleId:number){
     axios(`${BASE_URL}/sales/${saleId}/notification`).then(response=>{
-        console.log("Sucesso!");
+        toast.info("SMS enviado com sucesso!");
     });
 }
 
